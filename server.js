d234c448c82d3efbc17f5f09f2171f3171a8ba62
sonurepos <sonu.java123@outlook.com>
2022-05-01 18:05:48 +0530

    index.html path cha

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/phone-book-app-angular'));
+//app.use(express.static('./dist/phone-book-app-angular'));
 
 /*app.get('/*', (req, res) =>
     res.sendFile('index.html', { root: 'dist/phone-book-app-angular/' }),
 ); */
 
-app.get('/*', function (req, res) {
-    const fullPath = path.join(__dirname + '/dist/phone-book-app-angular/index.html');
-    console.log(" Fetching from.." + fullPath);
-    res.sendFile(fullPath);
-})
+if (process.env.NODE_ENV) {
+    app.use(express.static(path.resolve(process.cwd(), 'client/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(process.cwd(), 'client/build/index.html'))
+    })
+}
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
